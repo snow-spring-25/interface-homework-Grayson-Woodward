b6591b55798c1e08ef6e47288269f7c5cc505d33
Grayson Woodward <grayson.woodward@students.snow.edu>
2025-02-12 10:06:29 -0700

    changes

--- Fractional/RationalNumber.cs
+++ Fractional/RationalNumber.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Fractional
 {
-    public class RationalNumber
+    public class RationalNumber : IEquatable<RationalNumber>, IComparable<RationalNumber>
     {
         int numerator;
         int denominator;
-
         public RationalNumber(int num, int denom)
         {
+            if (denom == 0) throw new ArgumentException("Denominator cannot be zero.");
             numerator = num;
             denominator = denom;
 
@@ -20,13 +20,10 @@ namespace Fractional
             numerator /= gcd;
             denominator /= gcd;
         }
-
         public override string ToString()
         {
             return numerator + "/" + denominator;
         }
-
-        // (from Adam Fall 2020) 
         static int GreatestCommonDenominator(int a, int b)
         {
             if (b == 0)
@@ -37,6 +34,19 @@ namespace Fractional
             {
                 return GreatestCommonDenominator(b, a % b);
             }
+        }
+        public bool Equals(RationalNumber? other)
+        {
+            if (other == null) return false;
+            return this.numerator == other.numerator && this.denominator == other.denominator;
+        }
+        public int CompareTo(RationalNumber? other)
+        {
+            if (other == null) return 1; 
+            int left = this.numerator * other.denominator;
+            int right = this.denominator * other.numerator;
+
+            return left.CompareTo(right);
         }
     }
 }
